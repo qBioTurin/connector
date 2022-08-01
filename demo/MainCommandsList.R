b034bb7e8805bdb82a6655c0648ef2ca21defa9c
PerniceSimone <simone.pernice@unito.it>
2022-08-01 20:43:21 +0200

    Updating the docker image and the demo

--- demo/MainCommandsList.R
+++ demo/MainCommandsList.R
@@ -8,16 +8,17 @@ AnnotationFile <-system.file("data", "475treatedInfo.txt", package = "connector"
 CONNECTORList <- DataImport(TimeSeriesFile,AnnotationFile)
 
 ### Visualization
-
-GrowPlot<-GrowthCurve(CONNECTORList,"Progeny")
+# To plot just the longitudinal data
+GrowPlot<- PlotTimeSeries(CONNECTORList,"Progeny")
+GrowPlot$PlotTimeSeries_plot
+# To plot just the Time Grid 
 Timegrid <- TimeGridDensity(CONNECTORList)
-
+Timegrid$TimeGrid_plot
+# To visualize both the plots together
 Datavisual<-DataVisualization(CONNECTORList,
                               feature="Progeny",
                               labels = c("Time","Volume","Tumor Growth"))
-
 Datavisual
-
 
 ### Truncation
 CONNECTORList.trunc<- DataTruncation(CONNECTORList,
@@ -34,10 +35,11 @@ CrossLogLike$KnotsPlot
 # p is 
 p<-3
 
-#### New part:
-S.cl <-ClusterAnalysis(CONNECTORList.trunc,G=2:5,
+### Cluster Analysis to set G
+S.cl <-ClusterAnalysis(CONNECTORList.trunc,
+                       G=2:5,
                        p=p,
-                       runs=1,
+                       runs=50,
                        Cores=1)
 
 IndexesPlot.Extrapolation(S.cl)-> indexes
@@ -48,7 +50,6 @@ ConsInfo$G3$ConsensusPlot
 ConsInfo$G4$ConsensusPlot
 
 MostProbableClustering.Extrapolation(S.cl,4) ->MostProbableClustering
-
 
 FCMplots<- ClusterWithMeanCurve(clusterdata = MostProbableClustering,
                                feature = "Progeny",
